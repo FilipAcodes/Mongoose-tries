aeb8b82d838f80b170ef008afe8fd0c46850d2de
Filip Altankov <filipaltankov@hotmail.com>
2023-05-04 22:37:08 -0400

    groundwork

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hi");
+const mongoose = require("mongoose");
+
+const { MONGO_URI } = process.env;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+console.log(MONGO_URI);
+
+mongoose.connect(MONGO_URI, options).then(() => console.log("connected"));
