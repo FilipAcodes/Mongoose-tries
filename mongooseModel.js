23bcca89d7625c54802aee9085a6e87ddfe2ccdf
Filip Altankov <filipaltankov@hotmail.com>
2023-05-04 23:14:38 -0400

    initial work

--- mongooseModel.js
+++ mongooseModel.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  _id: String,
+  playlists: [
+    {
+      playlistname: String,
+      songs: [
+        {
+          songName: String,
+          artistName: String,
+          songId: Number,
+          picture: String,
+        },
+      ],
+    },
+  ],
+});
 
 const User = mongoose.model("User", userSchema);
 
--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 const mongoose = require("mongoose");
-
+const User = require("./mongooseModel");
+require("dotenv").config();
 const { MONGO_URI } = process.env;
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
-console.log(MONGO_URI);
 
-mongoose.connect(MONGO_URI, options).then(() => console.log("connected"));
+const test = async () => {
+  mongoose.connect(MONGO_URI, options);
+  const user = new User({
+    _id: "georgemichael@1234567",
+    playlists: [
+      { playlistname: "Potato" },
+      {
+        songs: [
+          {
+            songname: "hi",
+            artistname: "me",
+            songId: 420,
+            picture: "somepng",
+          },
+        ],
+      },
+    ],
+  });
+  await user.save();
+  console.log("User created:", user);
+  mongoose.connection.close();
+};
+
+test();
